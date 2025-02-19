fa7e8d836e3ada45521e4dba049cb2563c931040
Paulo Salvatore <salvatoregames@gmail.com>
2025-02-19 19:33:20 -0300

    Movimentação do projétil implementada

--- Assets/FireballController.cs
+++ Assets/FireballController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class FireballController : MonoBehaviour
 {
+    public float speed = 5;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,6 @@ public class FireballController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
     }
 }
