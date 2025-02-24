f52dbdde2534f402dd451da9d80e7a1a29d9b186
Paulo Salvatore <salvatoregames@gmail.com>
2025-02-24 19:20:59 -0300

    Movimentação do Nosferatu

--- Assets/NosferatuController.cs
+++ Assets/NosferatuController.cs
@@ -8,18 +8,44 @@ public class NosferatuController : MonoBehaviour
     // Tempo entre cada ataque
     public float delayToAttack = 1;
 
+    private Rigidbody2D rb;
+
+    private int dirX = -1;
+    private int dirY = 0;
+
+    public float intervalRandomDirection = 4f;
+    public float speed = 2f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Pegamos o componente do tipo Rigidbody2D que está no GameObject do Nosferatu
+        rb = GetComponent<Rigidbody2D>();
+
         // Executará o método "Shoot" uma vez a cada delayToAttack segundos
         InvokeRepeating(nameof(Shoot), 0, delayToAttack);
+
+        InvokeRepeating(nameof(SetRandomDirection), 0, intervalRandomDirection);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    void SetRandomDirection()
+    {
+        dirX = Random.Range(-1, 2);
+        dirY = Random.Range(-1, 2);
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(dirX * speed, dirY * speed);
+    }
+
+    // TODO: Saiu da Area, quero identificar e inverter a direção
 
     private void Shoot()
     {
